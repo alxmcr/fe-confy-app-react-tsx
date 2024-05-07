4e6581eb33643fe8b9b1cc0d745f502455a721ec
Alejandro Coca <alxmcr@outlook.com>
2024-05-07 18:04:09 -0400

    wip

--- src/components/footer/AppFooter.tsx
+++ src/components/footer/AppFooter.tsx
@@ -3,22 +3,20 @@ import Icon24x24Heart from '../@icons/24x24/Icon24x24Heart';
 
 export default function AppFooter() {
   return (
-    <footer className="">
-      <p className="">
-        <a
-          href="https://github.com/alxmcr/fe-confy-app-react-tsx"
-          target="_blank"
-          rel="noopener noreferrer"
-          className=""
-        >
-          <Icon24x24Github />
-        </a>
-        <span>
-          {` | Made with`}
-          <Icon24x24Heart />
-          {` in Latam`}
-        </span>
-      </p>
+    <footer className="flex h-[60px] items-center justify-center gap-2 bg-light-950 text-light-50">
+      <a
+        href="https://github.com/alxmcr/fe-confy-app-react-tsx"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+      >
+        <Icon24x24Github />
+      </a>
+      <span className="flex items-center gap-2">
+        {` | Made with `}
+        <Icon24x24Heart />
+        {` in Latam`}
+      </span>
     </footer>
   );
 }
