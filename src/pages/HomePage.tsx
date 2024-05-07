0e1f81b70be32aadb0bc53123803045b51edd00b
Alejandro Coca <alxmcr@outlook.com>
2024-05-07 00:01:25 -0400

    wip

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '../pages-layout/PageLayout';
+
+export default function HomePage() {
+  return (
+    <PageLayout>
+      <h1 className="font-open-sans text-3xl font-bold underline">Hello world!</h1>
+      <h2 className="font-bungee-inline text-3xl font-bold underline">Bungee world!</h2>
+    </PageLayout>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        light: {
+          50: '#ffffff',
+          100: '#efefef',
+          200: '#dcdcdc',
+          300: '#bdbdbd',
+          400: '#989898',
+          500: '#7c7c7c',
+          600: '#656565',
+          700: '#525252',
+          800: '#464646',
+          900: '#3d3d3d',
+          950: '#292929',
+        },
+        golden: {
+          50: '#fffaeb',
+          100: '#fff1c6',
+          200: '#ffe184',
+          300: '#ffcd4a',
+          400: '#ffb720',
+          500: '#f99407',
+          600: '#dd6d02',
+          700: '#b74b06',
+          800: '#94390c',
+          900: '#7a2f0d',
+          950: '#461702',
+        },
+        cerise: {
+          50: '#fef1f7',
+          100: '#fee5f0',
+          200: '#fecce3',
+          300: '#ffa2cb',
+          400: '#fe68a7',
+          500: '#f83c86',
+          600: '#e91f64',
+          700: '#ca0c47',
+          800: '#a70d3b',
+          900: '#8b1034',
+          950: '#55021a',
+        },
+      },
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'bungee-inline': ['Bungee Inline', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
