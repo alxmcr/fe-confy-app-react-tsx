f8fa96a465bb197c25a60036362b6279ab9f2eba
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 18:58:34 -0400

    wip

--- src/components/lists/TalkListItem.tsx
+++ src/components/lists/TalkListItem.tsx
@@ -11,12 +11,12 @@ export default function TalkListItem({ talk }: Props) {
       className="flex flex-col justify-start border-b border-light-50 py-3 text-light-50 md:flex-row md:items-center md:justify-between"
     >
       <div className="flex flex-col gap-4 md:flex-row md:items-center">
-        <span className="text-[20px] font-bold md:text-[22px]">{talk.time}</span>
-        <img className="size-[70px] rounded-lg" src={talk.speakerPhoto} alt={talk.speakerName} />
+        <span className="text-[1.25rem] font-bold md:text-[1.375rem]">{talk.time}</span>
+        <img className="size-[4.375rem] rounded-lg" src={talk.speakerPhoto} alt={talk.speakerName} />
       </div>
       <div className="flex flex-col md:items-end">
-        <span className="text-[18px]">{talk.speakerName}</span>
-        <h3 className="text-[28px] font-bold">{talk.title}</h3>
+        <span className="text-[1.125rem]">{talk.speakerName}</span>
+        <h3 className="text-[1.75rem] font-bold">{talk.title}</h3>
       </div>
     </div>
   );
