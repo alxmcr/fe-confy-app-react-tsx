1ef1675328365d3926c01ffd01d50ffa10f7b22a
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 00:12:21 -0400

    wip

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '../components/sections/HeroSection';
 import SpeakersSection from '../components/sections/SpeakersSection';
 import PageLayout from '../pages-layout/PageLayout';
 
 export default function HomePage() {
   return (
     <PageLayout>
+      <HeroSection />
       <SpeakersSection />
     </PageLayout>
   );
