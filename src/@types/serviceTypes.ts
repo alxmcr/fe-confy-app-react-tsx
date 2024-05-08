0467e0a34a19d2d5261b3ae83ecd7ac09130318e
Alejandro Coca <alxmcr@outlook.com>
2024-05-07 23:15:01 -0400

    wip

--- src/@types/serviceTypes.ts
+++ src/@types/serviceTypes.ts
@@ -3,6 +3,20 @@ export type SpeakerData = {
     name: string;
     value: string;
   };
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
   picture: {
     large: string;
     medium: string;
