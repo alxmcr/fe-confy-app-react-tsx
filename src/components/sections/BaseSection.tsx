22261ea8e256b7ee2149405cc47d67dd84c403a3
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 13:44:51 -0400

    wip

--- src/components/sections/BaseSection.tsx
+++ src/components/sections/BaseSection.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function BaseSection({ id = '', children }: Props) {
   return (
     <section id={id} className="flex h-screen justify-center bg-blue-500 p-4 md:p-10">
-      <div className="h-[500px] w-full bg-pink-500 lg:w-[1028px]">{children}</div>
+      <div className="w-full bg-pink-500 lg:w-[1028px]">{children}</div>
     </section>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,31 @@ export default function HomePage() {
     <div className="flex flex-col gap-2 bg-light-950">
       <AppHeader navLinks={MOCK_NAV_LINKS} />
       <BaseSection id="hero">
-        <div className="h-full bg-[url('/assets/images/eiffel-tower.jpg')] bg-cover bg-center bg-no-repeat"></div>
+        <div className="relative flex h-full flex-col items-center justify-center gap-4 bg-[url('/assets/images/eiffel-tower.jpg')] bg-cover bg-center bg-no-repeat px-2 py-3">
+          <div className="flex w-full flex-col gap-2 bg-golden-300 py-3">
+            <h1 className="text-center font-bungee-inline text-[60px]">Confy</h1>
+            <div className="flex flex-col gap-1">
+              <p className="text-center text-[20px]">
+                Online Tech Conference <span className="">October 4th., 2024</span>
+              </p>
+              <p className="text-center text-[18px]">París, France</p>
+            </div>
+          </div>
+          <div className="">
+            <a
+              href="#tickets"
+              className="flex h-[48px] w-[140px] items-center justify-center bg-golden-300 text-center text-[20px] font-semibold"
+            >
+              Tickets
+            </a>
+          </div>
+          <p className="absolute bottom-2 right-1 px-2 text-golden-300">
+            {`Photo by `}
+            <a href="https://www.pexels.com/photo/illuminated-eiffel-tower-at-night-18353583/">
+              Stephen Leonardi.
+            </a>
+          </p>
+        </div>
       </BaseSection>
       <BaseSection id="speakers">
         <p>Hola!</p>
