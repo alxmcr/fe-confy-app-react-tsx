3677bc67274fe3bab35c38c8d85f2c91d845ef49
Alejandro Coca <alxmcr@outlook.com>
2024-05-07 22:15:41 -0400

    wip

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -5,17 +5,19 @@ export default function HeroSection() {
       className="flex size-full justify-center bg-[url('/assets/images/eiffel-tower.jpg')] bg-cover bg-center bg-no-repeat py-4"
     >
       <div className="relative flex w-full flex-col items-center justify-center gap-3">
-        <div className="w-full bg-golden-300">
-          <h1 className="font-bungee-inline text-[54px]">Confy</h1>
-          <p className="">
-            Online Tech Conference <span className="">October 4th., 2021</span>
-          </p>
-          <p className="">París, France</p>
+        <div className="flex w-full flex-col gap-2 bg-golden-300 py-3">
+          <h1 className="text-center font-bungee-inline text-[60px]">Confy</h1>
+          <div className="flex flex-col gap-1">
+            <p className="text-center text-[20px]">
+              Online Tech Conference <span className="">October 4th., 2024</span>
+            </p>
+            <p className="text-center text-[18px]">París, France</p>
+          </div>
         </div>
         <div className="">
           <a
             href="#tickets"
-            className="flex h-[42px] w-[100px] items-center justify-center bg-golden-300 text-center font-semibold"
+            className="flex h-[48px] w-[140px] items-center justify-center bg-golden-300 text-center text-[20px] font-semibold"
           >
             Tickets
           </a>
--- src/pages-layout/PageLayout.tsx
+++ src/pages-layout/PageLayout.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function PageLayout({ children }: Props) {
   return (
-    <div className="border">
+    <div className="border bg-light-950">
       <div className="lg:mx-auto lg:w-[1028px]">
         <AppHeader navLinks={MOCK_NAV_LINKS} />
         <div className="h-screen px-[16px] md:px-[40px] lg:px-0">{children}</div>
