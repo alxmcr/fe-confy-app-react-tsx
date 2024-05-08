ad3fad3157ac96c29d29710268b9965d5c06f899
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 13:17:55 -0400

    wip

--- src/components/footer/AppFooter.tsx
+++ src/components/footer/AppFooter.tsx
@@ -3,8 +3,8 @@ import Icon24x24Heart from '../@icons/24x24/Icon24x24Heart';
 
 export default function AppFooter() {
   return (
-    <footer className="h-[60px] bg-light-950 p-4 text-light-50 md:p-10">
-      <div className="flex h-[40px] w-full items-center justify-center gap-2 bg-pink-500 lg:w-[1028px]">
+    <footer className="flex justify-center bg-blue-500 px-4 py-2 md:px-10 md:py-4">
+      <div className="flex h-[60px] w-full items-center justify-center gap-3 bg-pink-500 lg:w-[1028px]">
         <a
           href="https://github.com/alxmcr/fe-confy-app-react-tsx"
           target="_blank"
@@ -13,8 +13,9 @@ export default function AppFooter() {
         >
           <Icon24x24Github />
         </a>
+        <span>|</span>
         <span className="flex items-center gap-2">
-          {` | Made with `}
+          {`Made with `}
           <span className="text-golden-300">
             <Icon24x24Heart />
           </span>
