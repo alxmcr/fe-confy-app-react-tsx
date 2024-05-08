b47492efcbc470b7beca28768c01fef3c60deef0
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 13:29:09 -0400

    wip

--- src/components/sections/BaseSection.tsx
+++ src/components/sections/BaseSection.tsx
@@ -1,6 +1,10 @@
-export default function BaseSection() {
+type Props = {
+  id?: string;
+};
+
+export default function BaseSection({ id = '' }: Props) {
   return (
-    <section className="flex h-screen justify-center bg-blue-500 p-4 md:p-10">
+    <section id={id} className="flex h-screen justify-center bg-blue-500 p-4 md:p-10">
       <div className="h-[500px] w-full bg-pink-500 lg:w-[1028px]"></div>
     </section>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,8 +7,8 @@ export default function HomePage() {
   return (
     <div className="flex flex-col gap-2 bg-light-950">
       <AppHeader navLinks={MOCK_NAV_LINKS} />
-      <BaseSection />
-      <BaseSection />
+      <BaseSection id="hero" />
+      <BaseSection id="speakers" />
       <AppFooter />
     </div>
   );
