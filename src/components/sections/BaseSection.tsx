12bad15761ed3f6067ec107346f207f54fe59ce8
Alejandro Coca <alxmcr@outlook.com>
2024-05-08 13:31:00 -0400

    wip

--- src/components/sections/BaseSection.tsx
+++ src/components/sections/BaseSection.tsx
@@ -1,11 +1,12 @@
 type Props = {
   id?: string;
+  children: React.ReactNode;
 };
 
-export default function BaseSection({ id = '' }: Props) {
+export default function BaseSection({ id = '', children }: Props) {
   return (
     <section id={id} className="flex h-screen justify-center bg-blue-500 p-4 md:p-10">
-      <div className="h-[500px] w-full bg-pink-500 lg:w-[1028px]"></div>
+      <div className="h-[500px] w-full bg-pink-500 lg:w-[1028px]">{children}</div>
     </section>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,8 +7,12 @@ export default function HomePage() {
   return (
     <div className="flex flex-col gap-2 bg-light-950">
       <AppHeader navLinks={MOCK_NAV_LINKS} />
-      <BaseSection id="hero" />
-      <BaseSection id="speakers" />
+      <BaseSection id="hero">
+        <p>Hola!</p>
+      </BaseSection>
+      <BaseSection id="speakers">
+        <p>Hola!</p>
+      </BaseSection>
       <AppFooter />
     </div>
   );
